cc93e5505bb662d11aea9e812c59914e6a4d7d00
Jimmy Salazar <jimmy.salazar@tektonlabs.com>
2024-05-29 04:09:46 -0500

    feat: add tags

--- src/main/java/com/jimmysalazar/springmetrics/controller/TestMetrics.java
+++ src/main/java/com/jimmysalazar/springmetrics/controller/TestMetrics.java
@@ -1,5 +1,6 @@
 package com.jimmysalazar.springmetrics.controller;
 
+
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
@@ -11,21 +12,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/salazar/metrics")
 @Timed("salazar.timer") // a nivel de clase
 public class TestMetrics {
 
-    @Autowired
-    private MeterRegistry registry;
+    //@Autowired
+    //private MeterRegistry registry;
 
-    public static final Logger log = LoggerFactory.getLogger(TestMetrics.class);
+    //public static final Logger log = LoggerFactory.getLogger(TestMetrics.class);
 
     @GetMapping
     //@Timed("salazar.timer") // a nivel de método
     public ResponseEntity<String> get(){
-        log.info("MeterRegistry used {}", registry.getClass().getName());
+        //log.info("MeterRegistry used {}", registry.getClass().getName());
+
+        if(new Random().nextInt(20)==5){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not present");
+        }
+        if(new Random().nextInt(20)==7){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Bad request");
+        }
+        if(new Random().nextInt(20)==6){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Internal server error");
+        }
+
         //registry.counter("salazar.metrics").increment();
 
         // Sirve para medir un bloque de código en específico
